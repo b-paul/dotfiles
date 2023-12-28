99f201a3ecacaad05120c5ca8a023e4bdeaeb42e
Benjamin Paul <bpaul@bpaul.xyz>
2023-12-29 01:58:03 +1000

    delete nvim (im redoing the config and it will be in a separate repo)

--- nvim/.config/nvim/after/plugin/orgmode.lua
+++
@@ -1,8 +0,0 @@
-local orgmode = require('orgmode')
-
-orgmode.setup_ts_grammar()
-
-orgmode.setup({
-    org_agenda_files = { '~/doc/org/*' },
-    org_default_notes_file = '~/doc/org/index.org',
-})
--- nvim/.config/nvim/after/plugin/treesitter.lua
+++
@@ -1,9 +0,0 @@
-require 'nvim-treesitter.configs'.setup {
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = true,
-    },
-}
-
--- lol idk where else to put this
-vim.keymap.set('n', '<leader>p', require('nabla').popup, { noremap = true })
